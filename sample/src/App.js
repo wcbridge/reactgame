2724a8146de159d7cdf0684f5472ad831a363b0c
wcbridge <33330345+wcbridge@users.noreply.github.com>
2018-07-10 14:58:51 -0400

    still having trouble

--- sample/src/App.js
+++ sample/src/App.js
@@ -2,41 +2,59 @@ import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
+import Counter from "./components/score";
 import friends from "./friends.json";
 import "./App.css";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    friends,
+    count: 0
+    
   };
 
+
+  handleIncrement = () => {
+    // We always use the setState method to update a component's state
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  
   moveFriend = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
-    const clickedfriends = this.state.friends.filter(friend => friend.id === id);
-    
+    const clickedfriend = this.state.friends.slice(friend => friend.id === id);
+   
     // Set this.state.friends equal to the new friends array
     this.setState({ friends });
-    this.setState({clickedfriends});
+    this.setState({clickedfriend});
+    
+    //clickedfriends.concat(clickedfriend)
+    //console.log(clickfriend)
     console.log(friends)
-    console.log(clickedfriends)
-   friends.sort(function (a, b) { return 0.5 - Math.random() });
-  // newfriends.sort(function (a, b) { return 0.5 - Math.random() });
+    console.log(clickedfriend)
+    friends.sort(function () { return 0.5 - Math.random() });
   };
+
+  
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
+
     return (
-      <Wrapper>
-        <Title>Friends List</Title>
+      <Wrapper>          
+        <Title>Friends List <Counter count={this.state.count} /></Title>
+     
         {this.state.friends.map(friend => (
+
           <FriendCard
             moveFriend={this.moveFriend}
+            handleIncrement={this.handleIncrement}
             id={friend.id}
             key={friend.id}
             name={friend.name}
             image={friend.image}
-           
+
           />
         ))}
       </Wrapper>
--- sample/src/components/FriendCard/FriendCard.js
+++ sample/src/components/FriendCard/FriendCard.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./FriendCard.css";
 
+
 const FriendCard = props => (
   <div onClick={() => props.moveFriend(props.id)} className="card" >
+  {/* <div onClick={() => props.moveFriend(props.id)} className="card" > */}
+
     <div className="img-container">
       <img alt={props.name} src={props.image} />
-    </div>
-    <div className="content">        
-          <strong>Name:</strong> {props.name}
     </div>
   </div>
 );
